a214fe0f866cd67facfbdafb172ff576f90931eb
Garfield <dharmie19@gmail.com>
2021-10-23 19:02:57 +0100

    bug fixes

--- api/includes/Constants.php
+++ api/includes/Constants.php
@@ -6,10 +6,10 @@
 
 //Get Heroku ClearDB connection information
 // $cleardb_url = parse_url(getenv("mysql://b4b7c8a2ad059a:6267b6bf@us-cdbr-east-04.cleardb.com/heroku_0ad790080f03aa7?reconnect=true"));
-// define('CLEARDB_SERVER', $cleardb_url["host"]);
-// define('CLEARDB_USERNAME', $cleardb_url["user"]);
-// define('CLEARDB_PASSWORD', $cleardb_url["pass"]);
-// define('CLEARDB_DB', substr($cleardb_url["path"],1));
+ define('CLEARDB_SERVER', 'us-cdbr-east-04.cleardb.com');
+ define('CLEARDB_USERNAME', 'b4b7c8a2ad059a');
+define('CLEARDB_PASSWORD', '6267b6bf');
+define('CLEARDB_DB', 'heroku_0ad790080f03aa7');
 
     define('SECRET_KEY','test_sk_NzQxGXjwDo4x0S2VnVi4');
     
